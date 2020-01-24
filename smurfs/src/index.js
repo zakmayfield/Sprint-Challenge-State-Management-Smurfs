cf9e5a4f39fba0139614bebdb972b40dbf64ba46
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-24 11:21:48 -0500

    set up src/index.js -- created basic reducer and initialSttae with no data

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+
 import "./index.css";
 import App from "./components/App";
+import { reducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
